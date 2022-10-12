faca7877ffb3f98f6d0dc57002f7310fa88fdf25
Perlowicz <bartek.perlinski1234@gmail.com>
2022-10-12 20:20:59 +0200

    added working fractal

--- fractal.js
+++ fractal.js
@@ -1,20 +1,43 @@
-const firstPoint = [400,150];
-const secondPoint = [500,250];
-const thirdPoint = [300,50];
+const firstPoint = [200,150];// x1, y1
+const secondPoint = [100,400];// x2, y2
+const thirdPoint = [300,400];// x3, y3
 
 
 function setup() {
-    createCanvas(800,600);
+    createCanvas(800,600);// x , y
     noLoop();
 }
 
 function draw() {
-    //noprotect
     background(0);
 
-    stroke(white);
+    stroke('white');
 
     point(firstPoint[0],firstPoint[1]);
+    point(secondPoint[0],secondPoint[1]);
+    point(thirdPoint[0],thirdPoint[1]);
     
-    updatePixels();
+    var cx = firstPoint[0];
+    var cy = firstPoint[1];
+
+    for (i = 0; i < 30000; i++) {
+        var randomNumber = floor(random(0,3));
+
+        switch (randomNumber) {
+            case 0:
+                cx = (cx + firstPoint[0])/2;
+                cy = (cy + firstPoint[1])/2;
+                point(cx, cy);
+                break;
+            case 1:
+                cx = (cx + secondPoint[0])/2;
+                cy = (cy + secondPoint[1])/2;
+                point(cx,cy);
+                break;
+            default:
+                cx = (cx + thirdPoint[0])/2;
+                cy = (cy + thirdPoint[1])/2;
+                point(cx,cy);
+        }
+    }
 }
